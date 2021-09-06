a5eb435eef080f6b2c544b6b081007aaba23aabf
riskiirama <riskiramadhan09122001@gmail.com>
2021-09-06 11:21:00 +0700

    Complete

---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const double defaultMargin = 24.0;
+
+Color blueColor = Color(0xff3B3DBF);
+Color blackColor = Color(0xff161822);
+Color greyColor = Color(0xff737583);
+Color backgroundColor = Color(0xffF7F8FB);
+Color whiteColor = Color(0xffF7F8FB);
+
+TextStyle blackTextStyle = GoogleFonts.tajawal(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+);
+TextStyle whiteTextStyle = GoogleFonts.tajawal(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle greyTextStyle = GoogleFonts.tajawal(
+  color: greyColor,
+  fontWeight: FontWeight.w400,
+);
